e149887198f4246f15b3a5d9a609456107e1fe15
JerryXue0609 <JerryXue0609@foxmail.com>
2017-11-06 14:31:36 +0800

    测试控制层

---
+++ src/main/java/com/Jerry/JPAdemo/controller/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.Jerry.JPAdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Jerry on 2017/11/6 0006.
+ */
+@RestController
+public class HelloWorld {
+
+    @RequestMapping(value = "/hello")
+    public String hello(){
+        return "hello world";
+    }
+}
